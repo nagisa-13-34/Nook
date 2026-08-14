ed461bf3ea57637b743ce1d6d37039ea8a9d6a97
凪咲 <ambermedley1126@gmail.com>
2026-08-14 09:05:05 +0700

    Add Nook admin settings APIs

--- packages/backend/src/core/ModerationLogService.ts
+++ packages/backend/src/core/ModerationLogService.ts
@@ -6,11 +6,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DI } from '@/di-symbols.js';
 import type { ModerationLogsRepository } from '@/models/_.js';
+import { MiModerationLog } from '@/models/ModerationLog.js';
 import type { MiUser } from '@/models/User.js';
 import { IdService } from '@/core/IdService.js';
 import { bindThis } from '@/decorators.js';
 import type { ModerationLogPayloads } from '@/types.js';
 import { moderationLogTypes } from '@/types.js';
+import type { EntityManager } from 'typeorm';
 
 @Injectable()
 export class ModerationLogService {
@@ -23,8 +25,9 @@ export class ModerationLogService {
 	}
 
 	@bindThis
-	public async log<T extends typeof moderationLogTypes[number]>(moderator: { id: MiUser['id'] }, type: T, info?: ModerationLogPayloads[T]) {
-		await this.moderationLogsRepository.insert({
+	public async log<T extends typeof moderationLogTypes[number]>(moderator: { id: MiUser['id'] }, type: T, info?: ModerationLogPayloads[T], entityManager?: EntityManager) {
+		const repository = entityManager?.getRepository(MiModerationLog) ?? this.moderationLogsRepository;
+		await repository.insert({
 			id: this.idService.gen(),
 			userId: moderator.id,
 			type: type,
