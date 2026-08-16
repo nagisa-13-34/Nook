1ce9a55cba7d52068e073e3cb764b0d2f7c1a8c3
凪咲 <ambermedley1126@gmail.com>
2026-08-16 11:24:51 +0700

    Use type-only Misskey import

--- packages/frontend/src/pages/explore.videos.vue
+++ packages/frontend/src/pages/explore.videos.vue
@@ -40,7 +40,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <script lang="ts" setup>
 import { computed, onMounted, ref } from 'vue';
-import * as Misskey from 'misskey-js';
+import type * as Misskey from 'misskey-js';
 import MkButton from '@/components/MkButton.vue';
 import MkNote from '@/components/MkNote.vue';
 import MkTab from '@/components/MkTab.vue';
