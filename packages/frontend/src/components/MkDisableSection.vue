5943c273c7cd93171843c698b6ad2e44b0fee946
凪咲 <ambermedley1126@gmail.com>
2026-08-19 14:14:38 +0700

    Remove striped disabled sections

--- packages/frontend/src/components/MkDisableSection.vue
+++ packages/frontend/src/components/MkDisableSection.vue
@@ -24,19 +24,14 @@ defineProps<{
 }
 
 .disabled {
-	opacity: 0.3;
-	filter: saturate(0.5);
+	opacity: 0.42;
+	filter: grayscale(0.15);
 }
 
 .cover {
 	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
+	inset: 0;
 	cursor: not-allowed;
-	--color: light-dark(rgba(0, 0, 0, 0.05), rgba(255, 255, 255, 0.05));
-	background-size: auto auto;
-	background-image: repeating-linear-gradient(135deg, transparent, transparent 10px, var(--color) 4px, var(--color) 14px);
+	background: transparent;
 }
 </style>
