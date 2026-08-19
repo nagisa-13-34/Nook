dc8d3c73b1092690aa0f478b12480377478293c7
凪咲 <ambermedley1126@gmail.com>
2026-08-19 09:13:40 +0700

    Merge discover content into search

--- packages/frontend/src/pages/search.vue
+++ packages/frontend/src/pages/search.vue
@@ -22,25 +22,33 @@ SPDX-License-Identifier: AGPL-3.0-only
 			<MkInfo warn>{{ i18n.ts.usersSearchNotAvailable }}</MkInfo>
 		</div>
 	</div>
+
+	<div v-else-if="tab === 'featured'">
+		<XFeatured/>
+	</div>
+
+	<div v-else-if="tab === 'discoverUsers'">
+		<XExploreUsers/>
+	</div>
 </PageWithHeader>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineAsyncComponent, ref, toRef } from 'vue';
-import { $i } from '@/i.js';
+import { computed, defineAsyncComponent, ref, toRef, watch } from 'vue';
 import { i18n } from '@/i18n.js';
 import { definePage } from '@/page.js';
 import { notesSearchAvailable, usersSearchAvailable } from '@/utility/check-permissions.js';
 import MkInfo from '@/components/MkInfo.vue';
+
+type SearchDiscoverTab = 'note' | 'user' | 'featured' | 'discoverUsers';
 
 const props = withDefaults(defineProps<{
 	query?: string,
 	userId?: string,
 	username?: string,
 	host?: string | null,
-	type?: 'note' | 'user',
+	type?: SearchDiscoverTab,
 	origin?: 'combined' | 'local' | 'remote',
-	// For storybook only
 	ignoreNotesSearchAvailable?: boolean,
 }>(), {
 	query: '',
@@ -54,8 +62,14 @@ const props = withDefaults(defineProps<{
 
 const XNote = defineAsyncComponent(() => import('./search.note.vue'));
 const XUser = defineAsyncComponent(() => import('./search.user.vue'));
+const XFeatured = defineAsyncComponent(() => import('./explore.featured.vue'));
+const XExploreUsers = defineAsyncComponent(() => import('./explore.users.vue'));
 
-const tab = ref(toRef(props, 'type').value);
+const tab = ref<SearchDiscoverTab>(toRef(props, 'type').value);
+
+watch(() => props.type, (value) => {
+	if (value != null) tab.value = value;
+});
 
 const headerActions = computed(() => []);
 
@@ -66,11 +80,19 @@ const headerTabs = computed(() => [{
 }, {
 	key: 'user',
 	title: i18n.ts.users,
-	icon: 'ti ti-users',
+	icon: 'ti ti-search',
+}, {
+	key: 'featured',
+	title: i18n.ts.featured,
+	icon: 'ti ti-bolt',
+}, {
+	key: 'discoverUsers',
+	title: i18n.ts.nookDiscoverUsers,
+	icon: 'ti ti-users-plus',
 }]);
 
 definePage(() => ({
-	title: i18n.ts.search,
+	title: i18n.ts.nookSearchDiscover,
 	icon: 'ti ti-search',
 }));
 </script>
