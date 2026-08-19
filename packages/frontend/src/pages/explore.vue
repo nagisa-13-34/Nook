be2300ec0aadde3f0b6fb8219f108f0392f0335a
凪咲 <ambermedley1126@gmail.com>
2026-08-19 09:14:03 +0700

    Redirect legacy discover page to merged search

--- packages/frontend/src/pages/explore.vue
+++ packages/frontend/src/pages/explore.vue
@@ -4,62 +4,14 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<PageWithHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs" :swipable="true">
-	<div v-if="tab === 'featured'">
-		<XFeatured/>
-	</div>
-	<div v-else-if="tab === 'users'">
-		<XUsers/>
-	</div>
-	<div v-else-if="tab === 'roles'">
-		<XRoles/>
-	</div>
-</PageWithHeader>
+<MkLoading/>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from 'vue';
-import XFeatured from './explore.featured.vue';
-import XUsers from './explore.users.vue';
-import XRoles from './explore.roles.vue';
-import { i18n } from '@/i18n.js';
-import { definePage } from '@/page.js';
+import { onMounted } from 'vue';
+import { mainRouter } from '@/router.js';
 
-const props = withDefaults(defineProps<{
-	initialTab?: string;
-}>(), {
-	initialTab: 'featured',
+onMounted(() => {
+	mainRouter.replaceByPath('/search?type=featured');
 });
-
-function normalizeInitialTab(value: string): string {
-	if (value === 'spark' || value === 'theater' || value === 'shorts' || value === 'videos') return 'featured';
-	return value;
-}
-
-const tab = ref(normalizeInitialTab(props.initialTab));
-
-watch(() => props.initialTab, (value) => {
-	tab.value = normalizeInitialTab(value);
-});
-
-const headerActions = computed(() => []);
-
-const headerTabs = computed(() => [{
-	key: 'featured',
-	icon: 'ti ti-bolt',
-	title: i18n.ts.featured,
-}, {
-	key: 'users',
-	icon: 'ti ti-users',
-	title: i18n.ts.users,
-}, {
-	key: 'roles',
-	icon: 'ti ti-badges',
-	title: i18n.ts.roles,
-}]);
-
-definePage(() => ({
-	title: i18n.ts.explore,
-	icon: 'ti ti-hash',
-}));
 </script>
